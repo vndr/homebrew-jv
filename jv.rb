7f8a4bb6d03d8b8d18cbef5a2315c42f6f04443f
vndr <info@vndr.net>
2024-08-15 14:59:36 +0000

    Update JV formula to version v0.1.13

--- jv.rb
+++ jv.rb
@@ -1,9 +1,9 @@
 class Jv < Formula
     desc "JV IP Tool"
     homepage "https://github.com/vndr/jv"
-    url "https://github.com/vndr/jv/releases/download/v0.1.9/jv_Darwin_x86_64.tar.gz"
-    sha256 "d64dfcc517ad5c1464928975271e7af8440badf7f946f132f4ad2a07c1ae799d"
-    version "v0.1.9"
+    url "https://github.com/vndr/jv/releases/download/v0.1.13/jv_Darwin_x86_64.tar.gz"
+    sha256 "e42a45c0d5ea9de110d4d254e90f31a62abb05e7b60d0db65769c6d5aa0b4d5b"
+    version "v0.1.13"
   
     def install
       bin.install "jv"
@@ -24,15 +24,15 @@ class Jv < Formula
         url "https://github.com/vndr/jv/releases/download/v0.1.9/jv_Darwin_x86_64.tar.gz"
         sha256 "d64dfcc517ad5c1464928975271e7af8440badf7f946f132f4ad2a07c1ae799d"
       elsif Hardware::CPU.arm?
-        url "https://github.com/vndr/jv/releases/download/v0.1.9/jv_Darwin_arm64.tar.gz"
+        url "https://github.com/vndr/jv/releases/download/v0.1.13/jv_Darwin_arm64.tar.gz"
         sha256 "d64dfcc517ad5c1464928975271e7af8440badf7f946f132f4ad2a07c1ae799d"
       end
     elsif OS.linux?
       if Hardware::CPU.intel?
-        url "https://github.com/vndr/jv/releases/download/v0.1.9/jv_Linux_x86_64.tar.gz"
+        url "https://github.com/vndr/jv/releases/download/v0.1.13/jv_Linux_x86_64.tar.gz"
         sha256 "d64dfcc517ad5c1464928975271e7af8440badf7f946f132f4ad2a07c1ae799d"
       elsif Hardware::CPU.arm?
-        url "https://github.com/vndr/jv/releases/download/v0.1.9/jv_Linux_arm64.tar.gz"
+        url "https://github.com/vndr/jv/releases/download/v0.1.13/jv_Linux_arm64.tar.gz"
         sha256 "d64dfcc517ad5c1464928975271e7af8440badf7f946f132f4ad2a07c1ae799d"
       end
     end
